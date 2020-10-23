946072a9da7c1de271b4381f3da069049ad88135
nikita-skobov <nskobov.websites@gmail.com>
2020-10-22 23:21:03 -0500

    uses threading for counting max consec bits

--- nbint/src/bin/consec.rs
+++ nbint/src/bin/consec.rs
@@ -1,9 +1,13 @@
 use std::convert::From;
+use rayon::prelude::*;
 use binary_stuff::*;
 use die::*;
 
 const REQUIRED_NUM_ARGS: usize = 1;
 const DEFAULT_USE_BITS: &str = "zeros";
+/// I wrote this for my own processor, 16 max threads, so divide
+/// any file into 16 (if possible), TODO: make this a cli arg
+const MAX_FILE_SEGMENTS: usize = 16;
 
 #[derive(Copy, Clone)]
 pub enum CountBit {
@@ -46,15 +50,53 @@ pub fn get_max_data(data: &[u8], bit_count_type: CountBit) -> (usize, usize) {
         CountBitZeros => 0,
         CountBitOnes => 1,
     };
-    // TODO: do I want
-    // to add support for custom bin sizes? or
-    // just use whole file size?
-    binary_stuff::count_max_consecutive_bits(
-        data,
-        data.len() * 8,
-        0,
-        zero_or_one
-    )
+    // calculate a set of MAX_FILE_SEGMENTS, where each set
+    // contains the indices of the data such that the set spans
+    // the whole data. then use a rayon thread pool to iterate over
+    // these indices in parallel and cound the max consecutive bits
+    // of each file segment
+    let mut thread_set = vec![];
+    let data_bytes = data.len();
+    let bytes_per_segment = data_bytes / MAX_FILE_SEGMENTS;
+    for i in 0..MAX_FILE_SEGMENTS {
+        let byte_index = i * bytes_per_segment;
+        let bit_index = byte_index * 8;
+        let next_i = i + 1;
+        let next_segment_byte_index = next_i * bytes_per_segment;
+        let this_segment_bytes = next_segment_byte_index - byte_index;
+        let this_segment_bytes = if this_segment_bytes + byte_index > data_bytes {
+            data_bytes - byte_index
+        } else { this_segment_bytes };
+
+        let this_segment_bits = this_segment_bytes * 8;
+        thread_set.push((bit_index, this_segment_bits));
+    }
+    let joined_data: Vec<(usize, usize)> = thread_set.par_iter()
+        .map(|thread_data| {
+            // TODO: do I want
+            // to add support for custom bin sizes? or
+            // just use whole file size?
+            binary_stuff::count_max_consecutive_bits(
+                data,
+                thread_data.1,
+                thread_data.0,
+                zero_or_one
+            )
+        })
+        .collect();
+
+    // after parallel work is done and collected into a vec
+    // find the global max of all of the local maxes
+    let mut max_consec = 0;
+    let mut max_at = 0;
+    for (consec, at) in joined_data {
+        if consec > max_consec {
+            max_consec = consec;
+            max_at = at;
+        }
+    }
+
+    (max_consec, max_at)
 }
 
 fn main() {
